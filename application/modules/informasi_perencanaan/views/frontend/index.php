f1a435f3b8151cacf102c4bc8dcb121d2c0dd452
riosuprayogi <riosuprayogi98@gmail.com>
2021-05-10 09:14:28 +0700

    Perbaikan

--- application/modules/informasi_perencanaan/views/frontend/index.php
+++ application/modules/informasi_perencanaan/views/frontend/index.php
@@ -1,9 +1,10 @@
-<link rel="stylesheet" href="<?php echo base_url();?>assets/home2/mobirise/css/mbr-additional_ppid.css" type="text/css">
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/home2/mobirise/css/mbr-additional_ppid.css" type="text/css">
 
 <style>
-	.icon{
+	.icon {
 		width: 150px !important;
 	}
+
 	.card-horizontal {
 		display: flex;
 		flex: 1 1 auto;
@@ -17,7 +18,7 @@
                     <div class="media-container-row" >
                         <div class="mbr-figure rounded" style="padding:10px; background: rgba(255, 255, 255, 0.92); width: 40%;">
                             <div class="mbr-figure " style=" height:auto; opacity:1.0;">
-                                <img src="<?php echo base_url();?>assets/img/banner/PPID LOGO.png" style="width:50%; margin:auto;" alt="" media-simple="true">
+                                <img src="<?php echo base_url(); ?>assets/img/banner/PPID LOGO.png" style="width:50%; margin:auto;" alt="" media-simple="true">
                                 <p style="text-align: center; ">
                                 <?php echo $profil['isi']; ?>
                                     Informasi merupakan kebutuhan pokok setiap orang. Bahkan lebih mendasar, hak memperoleh informasi adalah salah satu dari hak asasi manusia, hal ini tercantum dalam Undang-Undang Dasar Negara Republik Indonesia Tahun 1945 Pasal 28 F. Dalam pasal tersebut disebutkan bahwa setiap orang berhak untuk berkomunikasi dan memperoleh informasi untuk mengembangkan pribadi dan lingkungan sosialnya, serta berhak untuk mencari, memperoleh, memiliki, dan menyimpan informasi dengan menggunakan segala jenis saluran yang tersedia. 
@@ -44,7 +45,7 @@
 	object-fit: cover;">
 </section>
 <section id="icon-formulir" class="cid-icon-formulir" data-rv-view="1620">
-	<div class="container">		
+	<div class="container">
 		<div class="media-container-row ">
 
 			<div class="row align-left">
@@ -53,7 +54,7 @@
 						<div class="card">
 							<div class="card-horizontal">
 								<div class="img-square-wrapper">
-									<!-- <img class="icon" src="<?php echo base_url();?>assets/img/icon/icon-10.png" alt="Card image cap"> -->
+									<!-- <img class="icon" src="<?php echo base_url(); ?>assets/img/icon/icon-10.png" alt="Card image cap"> -->
 									<img class="icon" src="<?= base_url('assets/media/image/rak.png') ?>" alt="Card image cap">
 
 								</div>
@@ -69,7 +70,7 @@
 						<div class="card">
 							<div class="card-horizontal">
 								<div class="img-square-wrapper">
-									<!-- <img class="icon" src="<?php echo base_url();?>assets/img/icon/icon-11.png" alt="Card image cap"> -->
+									<!-- <img class="icon" src="<?php echo base_url(); ?>assets/img/icon/icon-11.png" alt="Card image cap"> -->
 									<img class="icon" src="<?= base_url('assets/media/image/renstra.png') ?>" alt="Card image cap">
 								</div>
 								<div class="card-body">
@@ -88,7 +89,7 @@
 						<div class="card">
 							<div class="card-horizontal">
 								<div class="img-square-wrapper">
-									<!-- <img class="icon" src="<?php echo base_url();?>assets/img/icon/icon-12.png" alt="Card image cap"> -->
+									<!-- <img class="icon" src="<?php echo base_url(); ?>assets/img/icon/icon-12.png" alt="Card image cap"> -->
 									<img class="icon" src="<?= base_url('assets/media/image/dpa.png') ?>" alt="Card image cap">
 								</div>
 								<div class="card-body">
@@ -96,14 +97,14 @@
 								</div>
 							</div>
 						</div>
-					</a>	
+					</a>
 				</div>
 				<div class="col-md-6 ">
 					<a class="text-dark js-scroll-trigger" href="#dokumen" onclick="link_dokumen(16)">
 						<div class="card">
 							<div class="card-horizontal">
 								<div class="img-square-wrapper">
-									<!-- <img class="icon" src="<?php echo base_url();?>assets/img/icon/icon-13.png" alt="Card image cap"> -->
+									<!-- <img class="icon" src="<?php echo base_url(); ?>assets/img/icon/icon-13.png" alt="Card image cap"> -->
 									<img class="icon" src="<?= base_url('assets/media/image/renja.png') ?>" alt="Card image cap">
 								</div>
 								<div class="card-body">
@@ -122,7 +123,7 @@
 						<div class="card">
 							<div class="card-horizontal">
 								<div class="img-square-wrapper">
-									<img class="icon" src="<?php echo base_url();?>assets/img/icon/icon-15.png" alt="Card image cap">
+									<img class="icon" src="<?php echo base_url(); ?>assets/img/icon/icon-15.png" alt="Card image cap">
 								</div>
 								<div class="card-body">
 									<h4 class="card-title">Informasi Hukum dan Pemerintahan</h4>
@@ -136,7 +137,7 @@
 						<div class="card">
 							<div class="card-horizontal">
 								<div class="img-square-wrapper">
-									<img class="icon" src="<?php echo base_url();?>assets/img/icon/icon-14.png" alt="Card image cap">
+									<img class="icon" src="<?php echo base_url(); ?>assets/img/icon/icon-14.png" alt="Card image cap">
 								</div>
 								<div class="card-body">
 									<h4 class="card-title">Informasi Laporan Kinerja</h4>
@@ -154,7 +155,7 @@
 						<div class="card">
 							<div class="card-horizontal">
 								<div class="img-square-wrapper">
-									<img class="icon" src="<?php echo base_url();?>assets/img/icon/icon-16.png" alt="Card image cap">
+									<img class="icon" src="<?php echo base_url(); ?>assets/img/icon/icon-16.png" alt="Card image cap">
 								</div>
 								<div class="card-body">
 									<h4 class="card-title">Informasi Kepegawaian, Pendidikan dan Pelatihan</h4>
@@ -168,7 +169,7 @@
 						<div class="card">
 							<div class="card-horizontal">
 								<div class="img-square-wrapper">
-									<img class="icon" src="<?php echo base_url();?>assets/img/icon/icon-17.png" alt="Card image cap">
+									<img class="icon" src="<?php echo base_url(); ?>assets/img/icon/icon-17.png" alt="Card image cap">
 								</div>
 								<div class="card-body">
 									<h4 class="card-title">Data Informasi Statistik dan Publikasi</h4>
@@ -183,12 +184,12 @@
 		<!-- <div class="media-container-row ">
 			<div class="row">
 				<div class="col-sm-6 "> -->
-					<!-- ======== -->
-					<!-- <a class="text-dark js-scroll-trigger" href="#dokumen" onclick="link_dokumen(100)">
+		<!-- ======== -->
+		<!-- <a class="text-dark js-scroll-trigger" href="#dokumen" onclick="link_dokumen(100)">
 						<div class="card">
 							<div class="card-horizontal">
 								<div class="img-square-wrapper">
-									<img class="icon" src="<?php echo base_url();?>assets/img/icon/icon-09.png" alt="Card image cap">
+									<img class="icon" src="<?php echo base_url(); ?>assets/img/icon/icon-09.png" alt="Card image cap">
 								</div>
 								<div class="card-body">
 									<h4 class="card-title">Data dan Dokumen Pelayanan Informasi Publik</h4>
@@ -196,14 +197,14 @@
 							</div>
 						</div>
 					</a> -->
-					<!-- ======== -->
-				<!-- </div>
+		<!-- ======== -->
+		<!-- </div>
 				<div class="col-md-6 ">
 					<a class="text-dark js-scroll-trigger" href="#dokumen" onclick="link_dokumen(100)">
 						<div class="card">
 							<div class="card-horizontal">
 								<div class="img-square-wrapper">
-									<img class="icon" src="<?php echo base_url();?>assets/img/icon/icon-09.png" alt="Card image cap">
+									<img class="icon" src="<?php echo base_url(); ?>assets/img/icon/icon-09.png" alt="Card image cap">
 								</div>
 								<div class="card-body">
 									<h4 class="card-title">Data dan Dokumen Pelayanan Informasi Publik</h4>
@@ -215,7 +216,7 @@
 			</div>
 		</div>
 	</div> -->
-<!-- <div class="container">		
+		<!-- <div class="container">		
 	<div class="media-container-row my-3">
 		<div class="row align-left">
 			<div class="col-md-6">
@@ -223,7 +224,7 @@
 					<div class="card">
 						<div class="card-horizontal">
 							<div class="img-square-wrapper">
-								<img class="icon" src="<?php echo base_url();?>assets/img/icon/icon-06.png"
+								<img class="icon" src="<?php echo base_url(); ?>assets/img/icon/icon-06.png"
 									alt="Card image cap">
 							</div>
 							<div class="card-body">
@@ -238,7 +239,7 @@
 					<div class="card">
 						<div class="card-horizontal">
 							<div class="img-square-wrapper">
-								<img class="icon" src="<?php echo base_url();?>assets/img/icon/icon-09.png"
+								<img class="icon" src="<?php echo base_url(); ?>assets/img/icon/icon-09.png"
 									alt="Card image cap">
 							</div>
 							<div class="card-body">
@@ -257,7 +258,7 @@
 					<div class="card">
 						<div class="card-horizontal">
 							<div class="img-square-wrapper">
-								<img class="icon" src="<?php echo base_url();?>assets/img/icon/icon-07.png" alt="Card image cap">
+								<img class="icon" src="<?php echo base_url(); ?>assets/img/icon/icon-07.png" alt="Card image cap">
 							</div>
 							<div class="card-body">
 								<h4 class="card-title">Laporan Akses Informasi Publik Pemerintah Kota Tangerang</h4>
@@ -271,7 +272,7 @@
 						<div class="card">
 							<div class="card-horizontal">
 								<div class="img-square-wrapper">
-									<img class="icon" src="<?php echo base_url();?>assets/img/icon/icon-08.png" alt="Card image cap">
+									<img class="icon" src="<?php echo base_url(); ?>assets/img/icon/icon-08.png" alt="Card image cap">
 								</div>
 								<div class="card-body">
 									<h4 class="card-title">Daftar Informasi dan Dokumentasi Publik (DIDP)</h4>
@@ -291,26 +292,31 @@
 	<iframe id="iframe_dokumen" src="" frameborder="0" style="border: 0; height: 100%;left: 0;position: absolute;top: 0;width: 100%;"></iframe>
 </section>
 <section id="footer" style="display:none;">
-	
+
 </section>
 <style>
-	.col-md-6{
+	.col-md-6 {
 		margin-top: 5px !important;
 		margin-bottom: 5px !important;
 	}
+
 	@media only screen and (max-width: 768px) {
+
 		/* For mobile phones: */
 		#dokumen {
-			height:auto;
+			height: auto;
 			padding-bottom: 75% !important;
 		}
-		#footer{
+
+		#footer {
 			margin-top: 100px !important;
 		}
-		.col-md-6{
+
+		.col-md-6 {
 			margin-top: 5px !important;
 			margin-bottom: 5px !important;
 		}
+
 		.cid-icon-formulir {
 			padding-top: 2rem !important;
 			padding-bottom: 2rem !important;
@@ -320,17 +326,15 @@
 </style>
 
 <script>
-	function link_dokumen(id){
+	function link_dokumen(id) {
 		$("#dokumen").show();
 		$("#footer").show();
-		
+
 		// window.location.href = '<?= base_url('dokumen?key=') ?>'+id;
-		$("#iframe_dokumen").each(function(){
+		$("#iframe_dokumen").each(function() {
 			// $(this).attr("src", $(this).data("src"));
-			$(this).attr("src", '<?= base_url('dokumen?key=') ?>'+id);
-		}); 
-		
+			$(this).attr("src", '<?= base_url('dokumen?key=') ?>' + id);
+		});
+
 	}
-
-
 </script>
--- application/modules/pelayanan_langsung/views/backend/index.php
+++ application/modules/pelayanan_langsung/views/backend/index.php
@@ -153,7 +153,7 @@
 					},
 					{
 						field: 'deskripsi',
-						title: 'Deskripsi',
+						title: 'Pelayanan',
 						width: 480,
 						styler: function(value, row, index) {
 							if (value < 20) {
@@ -176,15 +176,18 @@
 							}
 						}
 					},
-					// {field:'urutan',title:'Urutan',
-					// 	styler: function(value,row,index){
-					// 	if (value < 20){
-					// 		// return 'background-color:#ffee00;color:red;';
-					// 		return 'min-width:100px';
-					// 		// the function can return predefined css class and inline style
-					// 		// return {class:'c1',style:'color:red'}
-					// 	}
-					// }},
+					{
+						field: 'urutan',
+						title: 'Urutan',
+						styler: function(value, row, index) {
+							if (value < 20) {
+								// return 'background-color:#ffee00;color:red;';
+								return 'min-width:100px';
+								// the function can return predefined css class and inline style
+								// return {class:'c1',style:'color:red'}
+							}
+						}
+					},
 					{
 						field: 'file',
 						title: 'FILE',
@@ -203,6 +206,7 @@
 						width: 150,
 						align: 'center'
 					}
+
 				]
 			]
 		});
@@ -1104,12 +1108,12 @@
 	<div class="container-fluid">
 		<div class="row mb-2">
 			<div class="col-sm-6">
-				<h1 class="m-0 text-dark">Manajemen Informasi Pengadaan</h1>
+				<h1 class="m-0 text-dark">Manajemen Pelayanan Langsung</h1>
 			</div>
 			<div class="col-sm-6">
 				<ol class="breadcrumb float-sm-right">
-					<!-- <li class="breadcrumb-item"><a href="<?php echo base_url(); ?>"">Home</a></li> -->
-					<li class="breadcrumb-item active">Informasi Pengadaan</li>
+					<li class="breadcrumb-item"><a href="<?php echo base_url(); ?>"">Home</a></li>
+              <li class=" breadcrumb-item active">Pelayanan Langsung</li>
 				</ol>
 			</div>
 		</div>
